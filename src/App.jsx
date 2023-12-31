c0282bd69a1d8c51f144bbf5a22fe52740c55994
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2023-12-31 12:57:22 +0330

    init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nftcard from './components/Nftcard'
+
+
+function App() {
+
+  return (
+    <main className='w-[23.4375rem] h-[41.6875rem] flex justify-center items-center text-white' style={{backgroundColor:"var(--very-dark-blue-main-bg, #0D192C)"}}>
+      <Nftcard />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Nftcard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Nftcard() {
+  return (
+    <div className='w-[20.4375rem] h-[33.9375rem] p-6 rounded-lg' 
+    style={{backgroundColor:"var(--very-dark-blue-card-bg, #15263F)"}}>
+        <div>
+            <img src='../images/Rectangle.png'/>
+            <h1 className='font-bold mt-4 text-[1.375rem]'>Equilibrium #3429</h1>
+            <p className='text-[#8BACD9] text-lg mt-2 mb-4'>Our Equilibrium collection promotes balance and calm.</p>
+            <div className='flex'>
+            <img className='mr-2' src='../images/icon-ethereum.svg'/><span className='text-[#00FFF8] text-[0.9375rem] font-semibold'>0.041 ETH</span>
+            <img className='ml-[5rem] mr-2' src='../images/icon-clock.svg'/><span className='text-[#8BACD9] text-[0.9375rem]'>3 days left</span>
+            </div>
+            <div className='w-[17.375rem] h-[0.0625rem] rounded-2xl mt-4'
+             style={{backgroundColor:"#8BACD9"}}></div>
+            <div className='flex mt-4'>
+                <img src='../images/Oval.svg' />
+                <p className='ml-4 text-[#8BACD9] text-[0.9375rem]'>Creation of<span className='ml-2 text-white' >Jules Wyvern</span></p>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
