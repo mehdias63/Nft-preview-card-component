e223704e1fa8bc4a29137784c3a53f3447d6aa6d
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2024-01-06 12:09:36 +0330

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Nftcard from './components/Nftcard'
 function App() {
 
   return (
-    <main className='w-[23.4375rem] h-[41.6875rem] flex justify-center items-center text-white' style={{backgroundColor:"var(--very-dark-blue-main-bg, #0D192C)"}}>
+    <main className='flex justify-center items-center text-white md:w-screen' style={{backgroundColor:"var(--very-dark-blue-main-bg, #0D192C)"}}>
       <Nftcard />
     </main>
   )
